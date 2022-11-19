606591b34ff07e38b3d8a92c52a12c5664b3ac5e
sabbirzzaman <sztonmoy7@gmail.com>
2022-11-19 13:14:11 +0600

    loading added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import Table from "./components/Table";
-import useUsers from "./hooks/useUsers";
+import Table from './components/Table';
+import useUsers from './hooks/useUsers';
+import { Oval } from 'react-loader-spinner';
 
 function App() {
-  const users = useUsers('https://jsonplaceholder.typicode.com/users');
+    // get user data form useUsers hook
+    const users = useUsers('https://jsonplaceholder.typicode.com/users');
 
-  return (
-    <div className="flex justify-center items-center w-screen h-screen text-gray-700 bg-gradient-to-tr from-blue-200 via-indigo-200 to-pink-200">
-      <Table />
-    </div>
-  );
+    const usersData = users[0] || [];
+
+    return usersData.length > 0 ? (
+        <div className="flex justify-center items-center w-screen h-screen text-gray-700 bg-gradient-to-tr from-blue-200 via-indigo-200 to-pink-200">
+            <Table users={usersData} />
+        </div>
+    ) : (
+        <div className="h-screen flex bg-violet-100 items-center justify-center">
+            <Oval
+                height={50}
+                width={50}
+                color="#5b21b6"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                ariaLabel="oval-loading"
+                secondaryColor="#ddd6fe"
+                strokeWidth={4}
+                strokeWidthSecondary={3}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-export default function Table() {
-  return (
-    <div className='sm:min-h-[400px] w-[725px] bg-white flex items-center justify-center rounded-lg shadow-lg py-8 px-4 sm:px-6 lg:px-8'>
-        
-    </div>
-  )
+export default function Table({users}) {
+    
+
+    return (
+        <div className="h-[400px] w-[725px] bg-white flex items-center justify-center rounded-lg shadow-lg py-8 px-4">
+            <div className="overflow-x-auto">
+                <table className="table table-compact w-full">
+                    <thead>
+                        <tr>
+                            <th className='bg-violet-200 text-violet-800 rounded-tl-lg rounded-bl-lg'>ID</th>
+                            <th className='bg-violet-200 text-violet-800'>Name</th>
+                            <th className='bg-violet-200 text-violet-800'>Username</th>
+                            <th className='bg-violet-200 text-violet-800'>Email</th>
+                            <th className='bg-violet-200 text-violet-800'>Phone</th>
+                            <th className='bg-violet-200 text-violet-800 rounded-tr-lg rounded-br-lg'>Website</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <td>Leanne Graham</td>
+                            <td>Bret</td>
+                            <td>Sincere@april.biz</td>
+                            <td>1-770-736-8031 x56442</td>
+                            <td>hildegard.org</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
